740a596be7c9595db54c6eed4a5653eea890f664
Quang Ngo <46818455+quangngonz@users.noreply.github.com>
2025-02-23 23:00:55 +0700

    Refactor StageItem component to use a separate StageItemSeparator for improved icon display based on objective completion

--- src/components/progressTimeline/stageItem/stageItem.tsx
+++ src/components/progressTimeline/stageItem/stageItem.tsx
@@ -1,39 +1,55 @@
 import {Accordion, AccordionDetails, AccordionSummary, Typography,} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
 
 import {Level} from '../../../types/objectives';
 import ObjectiveList from '../objectiveList/objectiveList';
 
+const StageItemSeparator = ({level}: { level: Level }) => {
+    const completed = level.objectives.every((obj) => obj.completed);
+    const partiallyCompleted = level.objectives.some((obj) => obj.completed);
+
+    if (completed) {
+        return <CheckBoxIcon color="success" fontSize={'large'}/>
+    } else if (partiallyCompleted) {
+        return <IndeterminateCheckBoxIcon color="info" fontSize={'large'}/>
+    } else {
+        return <CheckBoxOutlineBlankIcon fontSize={'large'}/>
+    }
+}
+
 const StageItem = (
     {level, isLast, isFirst}: { level: Level; isLast: boolean; isFirst: boolean
-}) => (
-    <TimelineItem >
-        <TimelineSeparator>
-            {isFirst ? <TimelineConnector style={{opacity : 0}} /> : <TimelineConnector />}
-            {level.completed ? (
-                <CheckCircleIcon color="success" />
-            ) : (
-                <CheckCircleIcon />
-            )}
-            {isLast ? <TimelineConnector style={{opacity : 0}} /> : <TimelineConnector />}
-        </TimelineSeparator>
-        <TimelineContent>
-            <Accordion style={{margin: '0'}}>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography variant="h6">{level.name}</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <ObjectiveList objectives={level.objectives} />
-                </AccordionDetails>
-            </Accordion>
-        </TimelineContent>
-    </TimelineItem>
-);
+}) => {
+
+    return (
+        <TimelineItem>
+            <TimelineSeparator>
+                {isFirst ? <TimelineConnector style={{opacity: 0}}/> : <TimelineConnector/>}
+                <StageItemSeparator level={level}/>
+                {isLast ? <TimelineConnector style={{opacity: 0}}/> : <TimelineConnector/>}
+            </TimelineSeparator>
+            <TimelineContent>
+                <Accordion style={{margin: '0'}}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                        <Typography variant="h6">{level.name}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <ObjectiveList objectives={level.objectives}/>
+                    </AccordionDetails>
+                </Accordion>
+            </TimelineContent>
+        </TimelineItem>
+    );
+}
 
 export default StageItem;
 
